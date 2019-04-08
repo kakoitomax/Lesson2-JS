33a6d536db36d2c4a66dc8444db41003322f5471
kakoitomax <max36598@gmail.com>
2019-04-08 13:41:34 +0200

    Lesson2-JS

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ else if (month >= 3 && month <=5) {
 else if (month >= 6 && month <=8) {
     console.log("Лето")
 }
-else if (month >= 9 && month <=11) {
+else (month >= 9 && month <=11) {
     console.log("Осень")
 }
 
